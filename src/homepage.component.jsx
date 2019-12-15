19db6d26eeea84af8b5e819f5e1aacb0042cfe89
djunh1 <djunh1@gmail.com>
2019-12-15 12:22:45 -0500

    Add component and scss

---
+++ src/homepage.component.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>
+            Hockey sticks - Kane
+          </h1>
+          <span className='subtitle'>
+            SHOP NOW
+          </span>
+        </div>
+      </div>
+
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>
+            Hockey sticks - Ovechkin
+          </h1>
+          <span className='subtitle'>
+            SHOP NOW
+          </span>
+        </div>
+      </div>
+
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>
+            Hockey sticks - Crosby
+          </h1>
+          <span className='subtitle'>
+            SHOP NOW
+          </span>
+        </div>
+      </div>
+
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>
+            Hockey sticks - Bergeron
+          </h1>
+          <span className='subtitle'>
+            SHOP NOW
+          </span>
+        </div>
+      </div>
+
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>
+            Hockey sticks - Pasternak
+          </h1>
+          <span className='subtitle'>
+            SHOP NOW
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
